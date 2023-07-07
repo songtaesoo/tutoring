117396b28dfe6fe03b015beee6e8afb651074d90
taesoo_song <taesoo_song@ecoinbank.cc>
2023-07-07 10:08:51 +0900

    mfsc 생성

---
+++ database/factories/SupportLanguageFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SupportLanguageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
--- database/seeders/SupportLanguageSeeder.php
+++ database/seeders/SupportLanguageSeeder.php
@@ -4,15 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class SupportLanguageSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        //
     }
 }
