cc716052544c20ef2e2d0ac48550b6d04d002f8c
songtaesoo <soong1298@gmail.com>
2023-07-08 17:57:52 +0900

    error fix

--- database/factories/TutoringFactory.php
+++ database/factories/TutoringFactory.php
@@ -17,17 +17,31 @@ class TutoringFactory extends Factory
      */
     public function definition()
     {
+        $status = $this->faker->randomElement(['pending', 'processing', 'completed', 'disconnected', 'cancelled', 'reserved']);
+
+        switch ($status){
+            case "pending":
+                $started_at = null;
+                $ended_at = null;
+            case "processing":
+                $started_at = $this->faker->dateTimeBetween('-1 hour', 'now');
+                $ended_at = null;
+            case "completed":
+            case "disconnected":
+            case "cancelled":
+            case "reserved":
+                $started_at = $this->faker->dateTimeBetween('-2 hour', 'now');
+                $ended_at = $this->faker->dateTimeBetween('-1 hour', 'now');
+                break;
+        }
+
         return [
             'student_id' => Student::inRandomOrder()->first()->id,
             'tutor_id' => Tutor::inRandomOrder()->first()->id,
             'course_id' => Course::inRandomOrder()->first()->id,
-            'status' => $this->faker->randomElement(['pending', 'processing', 'completed', 'disconnected', 'cancelled']),
-            'started_at' => $this->faker->optional(function ($value){
-                return in_array($value['status'], ['processing', 'completed', 'disconnected']);
-            })->dateTimeBetween('-1 hour', 'now'),
-            'ended_at' => $this->faker->optional(function ($value){
-                return in_array($value['status'], ['completed', 'cancelled', 'disconnected']);
-            })->dateTimeBetween('now', '+1 hour'),
+            'status' => $status,
+            'started_at' => $started_at,
+            'ended_at' => $ended_at,
             'description' => ''
         ];
     }
